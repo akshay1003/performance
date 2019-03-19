7bf7f24608f3c36c947d1f3ce7ee0d40662b1bad
aksahy1003 <akshay.pathak1003@gmail.com>
2019-03-19 17:59:09 +0530

    making more changes in CalcTest java file

--- src/test/java/com/qaagility/controller/CalcTest.java
+++ src/test/java/com/qaagility/controller/CalcTest.java
@@ -23,8 +23,9 @@ public class CalcTest {
 
         Calcmul testcalmul = new Calcmul();
         Calculator testcaladd= new Calculator();
+
         assertEquals(testcalmul.mul(),18);
-        assertEquals(testcaladd.add() 9);
+        assertEquals(testcaladd.add(),9);
       }
 
 	@Test	
